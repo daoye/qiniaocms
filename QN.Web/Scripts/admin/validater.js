1018797f966d6cf1a02cf1914dd0836b1073f550
daoye <nozeryu@yeah.net>
2015-01-28 21:59:37 +0800

    调整图册封面图片大小

---
+++ QN.Web/Scripts/admin/validater.js
@@ -0,0 +1,19 @@
+﻿
+(function ($) {
+    var $jQval = $.validator;
+
+    $jQval.addMethod('qrequired', function (value, element, params) {
+        return $jQval.methods.required(value, element, params);
+    });
+
+    $jQval.unobtrusive.adapters.add('qrequired', function (options) {
+        if (options.element.tagName.toUpperCase() !== "INPUT" || options.element.type.toUpperCase() !== "CHECKBOX") {
+            var ruleName = "required";
+
+            options.rules[ruleName] = true;
+            if (options.message) {
+                options.messages[ruleName] = options.message;
+            }
+        }
+    });
+}(jQuery));
